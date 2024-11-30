621f31fea4b18287027d2b62c5470a773bdb1781
Toushik-Ahmed <Pronay5271@gmail.com>
2024-11-30 23:36:22 +0600

    keys

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,25 +1,16 @@
-import {
-  App,
-  cert,
-  getApp,
-  getApps,
-  initializeApp
-} from 'firebase-admin/app';
+import { App, cert, getApp, getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-
-
-
-const serviceKey = require('./service_key.json');
-let app: App;
-
-if (getApps().length === 0) {
-  app = initializeApp({
-    credential: cert(serviceKey),
-  });
-} else {
-  app = getApp();
-}
+const app: App =
+  getApps().length === 0
+    ? initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        }),
+      })
+    : getApp();
 
 const adminDb = getFirestore(app);
 export { app as adminApp, adminDb };
